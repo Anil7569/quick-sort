26ef4f21983fc8af43feb27422946223d444a227
Anil Gorantala <Anil_Gorantala@epam.com>
2017-07-12 15:10:09 +0530

    quick sort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -2,69 +2,31 @@
 
 function quickSort(input) {
     if (Array.isArray(input) && input.length >= 0) {
-        if (input.length == 1) {
+        if (input.length == 0 || input.length == 1) {
             return input;
         } else {
-            quickSorter(input, 0, input.length - 1);
+            var left = [];
+            var right = [];
+            var newArray = [];
+            var pivot = input.pop();
+            var length = input.length;
+
+            for (var i = 0; i < length; i++) {
+                if (input[i] <= pivot) {
+                    left.push(input[i]);
+                } else {
+                    right.push(input[i]);
+                }
+            }
+
+            return newArray.concat(quickSort(left), pivot, quickSort(right));
         }
     } else {
         if (!Array.isArray(input)) {
             throw "Input is not an array.";
         }
     }
-};
-
-function partition(items, left, right) {
-
-    var pivot = items[Math.floor((right + left) / 2)],
-        i = left,
-        j = right;
+}
 
 
-    while (i <= j) {
-
-        while (items[i] < pivot) {
-            i++;
-        }
-
-        while (items[j] > pivot) {
-            j--;
-        }
-
-        if (i <= j) {
-            swap(items, i, j);
-            i++;
-            j--;
-        }
-    }
-
-    return i;
-};
-
-function swap(items, firstIndex, secondIndex) {
-    var temp = items[firstIndex];
-    items[firstIndex] = items[secondIndex];
-    items[secondIndex] = temp;
-};
-
-function quickSorter(items, left, right) {
-
-    var index;
-
-    if (items.length > 1) {
-
-        index = partition(items, left, right);
-
-        if (left < index - 1) {
-            quickSorter(items, left, index - 1);
-        }
-
-        if (index < right) {
-            quickSorter(items, index, right);
-        }
-
-    }
-
-    return items;
-};
 module.exports = quickSort;
